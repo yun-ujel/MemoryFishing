d1923d30545891ffc8bb26f27af5f4c59ed67cba
yun-ujel <dhall2022@elam.co.uk>
2024-05-07 15:15:52 +0100

    Updated FollowPosition

--- Assets/Scripts/FX/FollowPosition.cs
+++ Assets/Scripts/FX/FollowPosition.cs
@@ -10,24 +10,48 @@ namespace MemoryFishing.FX
 
         [Space]
 
-        [SerializeField] private bool trackPosition;
-        [SerializeField] private bool trackRotation;
+        [Header("Track Position")]
+        [SerializeField] private bool trackXPosition;
+        [SerializeField] private bool trackYPosition;
+        [SerializeField] private bool trackZPosition;
 
-        [Space]
+        private bool TrackPosition => trackXPosition || trackYPosition || trackZPosition;
+        private Vector3 TargetPos => follow.position;
 
-        [SerializeField] private Vector3 offset;
+        [Space, SerializeField] private Vector3 positionOffset;
+
+        [Header("Track Rotation")]
+        [SerializeField] private bool trackXRotation;
+        [SerializeField] private bool trackYRotation;
+        [SerializeField] private bool trackZRotation;
+
+        private bool TrackRotation => trackXRotation || trackYRotation || trackZRotation;
+        private Vector3 TargetEulers => follow.rotation.eulerAngles;
+
+        [Space, SerializeField] private Vector3 rotationOffset;
 
         private void Update()
         {
-            if (trackPosition)
+            if (TrackPosition)
             {
-                Vector3 pos = follow.position + offset;
+                Vector3 pos = positionOffset;
+
+                if (trackXPosition) { pos.x += TargetPos.x; }
+                if (trackYPosition) { pos.y += TargetPos.y; }
+                if (trackZPosition) { pos.z += TargetPos.z; }
+
                 transform.position = pos;
             }
 
-            if (trackRotation)
+            if (TrackRotation)
             {
-                transform.rotation = follow.rotation;
+                Vector3 euler = rotationOffset;
+
+                if (trackXRotation) { euler.x += TargetEulers.x; }
+                if (trackYRotation) { euler.y += TargetEulers.y; }
+                if (trackZRotation) { euler.z += TargetEulers.z; }
+
+                transform.rotation = Quaternion.Euler(euler);
             }
         }
     }
